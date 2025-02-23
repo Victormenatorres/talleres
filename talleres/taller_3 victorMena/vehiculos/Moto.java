01dbc48e5df6b685135446e61df6cf8fa78d3d48
Victormenatorres <victor.menatorres@unicolombo.edu.co>
2025-02-23 15:18:28 -0500

    Add files via upload

---
+++ talleres/taller_3 victorMena/vehiculos/Moto.java
@@ -0,0 +1,17 @@
+package vehiculos;
+
+public class Moto extends Vehiculo {
+
+    Moto() {
+        super("Moto"); 
+    }
+
+    void mostrarInfo() {
+        System.out.println("Esta es una moto.");
+        mostrarTipo(); 
+    }
+
+    
+}
+    
+    
